28a3a0ae61db9ce6a861e87f2bb62d8588396317
equant <nathanhendler@gmail.com>
2021-10-11 11:41:54 -0700

    updated current script to use phytooracle_data and .env

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from json import load
 from cv2 import boundingRect
 from utils import *
 from data import *
+from config import Config
 
 def main():
-    orth_path = '/home/ariyan/Desktop/LandmarkSelection/2020-03-01_ortho_10pct_cubic.tif'
+
+    #orth_path = '/home/ariyan/Desktop/LandmarkSelection/2020-03-01_ortho_10pct_cubic.tif'
+
+    season = 10
+    conf = Config(season)
+    date = "2020-03-01"
+    orth_path = conf.ortho.get_ortho_for_date(date)
+
     boundaries = read_tags(orth_path)
     print(get_gps_distance(boundaries['UL'],boundaries['UR']))
     print(get_gps_distance(boundaries['UL'],boundaries['LL']))
